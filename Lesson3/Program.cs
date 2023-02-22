cb0bcdd559969c68447c988d3152be9f9a652150
«anton» <kaluta99@gmail.com>
2023-02-23 01:09:39 +0700

    Все прорешено

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -83,8 +83,77 @@
 // string newText4 = Replace(newText3, 'С', 'с');
 // System.Console.WriteLine(newText4);
 
-// Сортировка массива
+// Сортировка массива от меньшего к большему
 
-int[] arr = { 1, 2, 5, 7, 8, 9, 2, 4, 5 };
+// int[] arr = { 1, 2, 5, 7, 8, 9, 2, 4, 5, 9, 2 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     System.Console.WriteLine();
+
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
 
 
+// Сортировка от большего к меньшему
+
+// int[] arr = { 1, 2, 5, 7, 8, 9, 2, 4, 5, 9, 2 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+
+//     for (int i = 0; i < count; i++)
+//     {
+//         System.Console.Write($"{array[i]} ");
+//     }
+//     System.Console.WriteLine();
+
+// }
+
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int maxPosition = i;
+
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] > array[maxPosition]) maxPosition = j;
+//         }
+
+//         int temporary = array[i];
+//         array[i] = array[maxPosition];
+//         array[maxPosition] = temporary;
+//     }
+
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
